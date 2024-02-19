ac35e4dd06e6b143d1198b59112bfc320e9daa79
afr117 <alfredo.figueroa07@yahoo.com>
2024-02-19 01:57:19 +0000

    task1 update

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,9 +1,3 @@
---use a workaround to create the database if it doesn't exist
+--create a database hbtn_0c_0 if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
-
--- Create a dummy table only if the database was just created
-CREATE TABLE IF NOT EXISTS hbtn_0c_0.dummy_table (id INT);
-
--- Drop the dummy table if it was created in this session
-DROP TABLE IF EXISTS hbtn_0c_0.dummy_table;
 
