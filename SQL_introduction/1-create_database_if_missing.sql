c379a913c6e08b61111df9408f5d0454877a7026
afr117 <alfredo.figueroa07@yahoo.com>
2024-02-19 01:53:55 +0000

    task1 update

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,24 +1,9 @@
--- Create a stored procedure to create the database if it doesn't exist
-DELIMITER //
+--use a workaround to create the database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
 
-CREATE PROCEDURE create_database_if_missing()
-BEGIN
-    DECLARE db_count INT;
+-- Create a dummy table only if the database was just created
+CREATE TABLE IF NOT EXISTS hbtn_0c_0.dummy_table (id INT);
 
-    -- Check if the database exists
-    SELECT COUNT(*)
-    INTO db_count
-    FROM information_schema.SCHEMATA
-    WHERE SCHEMA_NAME = 'hbtn_0c_0';
-
-    -- If the database doesn't exist, create it
-    IF db_count = 0 THEN
-        CREATE DATABASE hbtn_0c_0;
-    END IF;
-END //
-
-DELIMITER ;
-
--- Call the stored procedure to create the database if it doesn't exist
-CALL create_database_if_missing();
+-- Drop the dummy table if it was created in this session
+DROP TABLE IF EXISTS hbtn_0c_0.dummy_table;
 
