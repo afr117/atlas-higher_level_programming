099b918fcc3ab31acf8de2c4409fb05142493015
afr117 <54492982+afr117@users.noreply.github.com>
2024-02-20 12:12:36 -0500

    Update 3-force_name.sql

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,7 +1,5 @@
 -- Create the table force_name if not exists
-USE ?;
 CREATE TABLE IF NOT EXISTS force_name (
-	    id INT,
-	    name VARCHAR(256) NOT NULL
-	);
-
+    id INT,
+    name VARCHAR(256) NOT NULL
+);
