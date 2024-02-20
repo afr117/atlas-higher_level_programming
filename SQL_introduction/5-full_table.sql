b8787ef7db0dbdacff1774c381eead0f8fc17267
afr117 <54492982+afr117@users.noreply.github.com>
2024-02-20 15:32:49 -0500

    Update 5-full_table.sql

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,8 +1,7 @@
-e the specified database
+-- e the specified database
 USE hbtn_0c_0;
 
 -- Query the information schema to retrieve table structure
 SELECT CONCAT('Table', '   ', 'Create Table') AS 'Table   Create Table'
 FROM information_schema.TABLES
 WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
-
