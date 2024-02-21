5978a22d49ca819f3b7e9496c7b1e8d29ca41a9d
afr117 <54492982+afr117@users.noreply.github.com>
2024-02-20 21:31:29 -0500

    Create 3-my_safe_filter_states.py

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+Script that takes in arguments and displays all values in the states table
+of hbtn_0e_0_usa where name matches the argument (safe from MySQL injection).
+"""
+
+import sys
+import MySQLdb
+
+
+def filter_states(username, password, database, state_name):
+    try:
+        # Connect to MySQL server
+        db = MySQLdb.connect(host="localhost", port=3306,
+                             user=username, passwd=password, db=database)
+        cursor = db.cursor()
+
+        # Execute SQL query with parameters to prevent SQL injection
+        query = ("SELECT * FROM states WHERE name LIKE BINARY %s "
+                 "ORDER BY id ASC")
+        cursor.execute(query, (state_name,))
+        states = cursor.fetchall()
+
+        # Print results
+        for state in states:
+            print(state)
+
+        # Close cursor and database connection
+        cursor.close()
+        db.close()
+    except MySQLdb.Error as e:
+        print("MySQL Error:", e)
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        print("Usage: {} <username> <password> <database> <state_name>"
+              .format(sys.argv[0]))
+        sys.exit(1)
+
+    username, password, database, state_name = sys.argv[1], sys.argv[2], \
+        sys.argv[3], sys.argv[4]
+    filter_states(username, password, database, state_name)
