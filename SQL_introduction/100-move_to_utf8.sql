1bc1df9788eab430fc05ff09bb0511a7b2972a2c
afr117 <54492982+afr117@users.noreply.github.com>
2024-02-21 13:04:11 -0500

    Update 100-move_to_utf8.sql

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,6 +1,11 @@
+-- Change to the hbtn_0c_0 database
 USE hbtn_0c_0;
 
--- Convert hbtn_0c_0 database to UTF8
+-- Alter the database character set and collation to utf8mb4
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Alter the character set and collation of the first_table to utf8mb4
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Modify the name column in the first_table to use utf8mb4 character set and collation
 ALTER TABLE first_table MODIFY name VARCHAR(256) COLLATE utf8mb4_unicode_ci DEFAULT NULL;
