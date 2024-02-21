21e66c628362baef359b387db84b65fdaa91a09a
afr117 <54492982+afr117@users.noreply.github.com>
2024-02-21 10:22:21 -0500

    Update 10-top_score.sql

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,8 +1,4 @@
--- Use the hbtn_0c_0 database
-USE hbtn_0c_0;
-
 -- List all records of the second_table ordered by score (top first) and display score and name
 SELECT score, name
 FROM second_table
 ORDER BY score DESC;
-
