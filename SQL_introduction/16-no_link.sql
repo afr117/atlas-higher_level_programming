6ec7a0f4e663a61eeb4fdcc23bcfce23b542786a
afr117 <54492982+afr117@users.noreply.github.com>
2024-02-21 10:25:47 -0500

    Update 16-no_link.sql

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,9 +1,5 @@
--- Use the hbtn_0c_0 database
-USE hbtn_0c_0;
-
 -- List all records with a name value, ordered by descending score
 SELECT score, name
 FROM second_table
 WHERE name IS NOT NULL
 ORDER BY score DESC;
-
