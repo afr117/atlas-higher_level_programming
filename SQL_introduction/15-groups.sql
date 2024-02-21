65f91e8a2f742b56366b6795fa2acef35d99f22b
afr117 <54492982+afr117@users.noreply.github.com>
2024-02-21 10:25:36 -0500

    Update 15-groups.sql

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,9 +1,5 @@
--- Use the hbtn_0c_0 database
-USE hbtn_0c_0;
-
 -- List the number of records with the same score
 SELECT score, COUNT(*) AS number
 FROM second_table
 GROUP BY score
 ORDER BY number DESC;
-
