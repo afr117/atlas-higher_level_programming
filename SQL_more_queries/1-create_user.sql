e66d49bbe340f7fd7518c96771ef4efa5d969eab
afr117 <alfredo.figueroa07@yahoo.com>
2024-06-20 01:22:32 +0000

    t2 correct out ? load

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,6 +1,3 @@
--- 1-create_user.sql
--- Creates MySQL user user_0d_1 with all privileges and sets password to user_0d_1_pwd
-
 -- Create user_0d_1 if it does not exist
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
