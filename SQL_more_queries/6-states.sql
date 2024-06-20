5b3b6ab640c2c2a0e14227e34a3aca472aaffe9d
afr117 <alfredo.figueroa07@yahoo.com>
2024-06-20 01:35:21 +0000

    t6 no error load

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,12 @@
+-- Create database hbtn_0d_usa if it does not exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- Use the database hbtn_0d_usa
+USE hbtn_0d_usa;
+
+-- Create table states if it does not exist
+CREATE TABLE IF NOT EXISTS states (
+    id INT AUTO_INCREMENT UNIQUE,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
+);
