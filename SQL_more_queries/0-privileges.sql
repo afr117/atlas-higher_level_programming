019857b99d995564c4fc614e2ef4069dc0a1ad4f
afr117 <alfredo.figueroa07@yahoo.com>
2024-06-20 00:52:19 +0000

    if user

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,16 +1,5 @@
-
--- Check if user_0d_1 exists, create if not
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
--- Grant all privileges to user_0d_1
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-
--- Check if user_0d_2 exists, create if not
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
--- Grant all privileges to user_0d_2
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
-
--- List all privileges for user_0d_1
+-- Attempt to show privileges for user_0d_1
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 
--- List all privileges for user_0d_2
+-- Attempt to show privileges for user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
