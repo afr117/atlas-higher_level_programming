7006f9366d201ad5d9262f3e98fb3e924ac5b3ed
afr117 <alfredo.figueroa07@yahoo.com>
2024-06-20 01:17:35 +0000

    t1 test load

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,12 @@
+-- 1-create_user.sql
+-- Creates MySQL user user_0d_1 with all privileges and sets password to user_0d_1_pwd
+
+-- Create user_0d_1 if it does not exist
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all privileges to user_0d_1
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+
+-- Apply the changes
+FLUSH PRIVILEGES;
+
