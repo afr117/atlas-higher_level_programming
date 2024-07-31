947e378acbc12fc92fcd6fef98ca9be185784b3f
afr117 <alfredo.figueroa07@yahoo.com>
2024-07-31 18:49:27 +0000

    t4 correct print

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+const request = require('request');
+
+const apiUrl = process.argv[2];
+const wedgeAntillesId = '18';
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else if (response.statusCode === 200) {
+    const films = JSON.parse(body).results;
+    let count = 0;
+
+    films.forEach(film => {
+      if (film.characters.includes(`https://swapi-api.hbtn.io/api/people/${wedgeAntillesId}/`)) {
+        count++;
+      }
+    });
+
+    console.log(count);
+  } else {
+    console.log(`Error code: ${response.statusCode}`);
+  }
+});
